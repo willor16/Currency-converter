aef3d7b75837dae2273ccbe0bfb9ae565734385b
wilmer_choxom <lorwilm@gmail.com>
2024-06-18 00:45:09 -0600

    agregando conexion con la api

--- CurrencyConverter/src/com/wilmer/currencyconverter/conections/ApiConexion.java
+++ CurrencyConverter/src/com/wilmer/currencyconverter/conections/ApiConexion.java
@@ -1,2 +1,32 @@
-package com.wilmer.currencyconverter.conections;public class ApiConexion {
+package com.wilmer.currencyconverter.conections;
+
+import com.google.gson.Gson;
+import com.wilmer.currencyconverter.Main.Main;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+
+public class ApiConexion {
+    public ValuesMoney typeMoney(String type){
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://v6.exchangerate-api.com/v6/7ae484cde71b4b7d8736b8d5/latest/"+type))
+                .build();
+        HttpResponse<String> response;
+        try {
+             response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return new Gson().fromJson(response.body(), ValuesMoney.class);
+    }
+
+
 }
